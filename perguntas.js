82f532779cf34227b1c509b2a989f9a4500abb77
juthomaz <juliabuenothomaz@gmail.com>
2024-11-28 07:50:37 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,30 @@
+criaCartao(
+    'Veterinária',
+    'O que é um profissional em Veterinária?',
+    'É o profissional que atua pela saúde e pelo bem-estar dos animais, dos seres humanos e pela sustentabilidade do meio ambiente.'
+)
+
+criaCartao(
+    'Veterinária',
+    'Qual a quantidade de ossos de um cachorro?',
+    'Possui entre 319 e 323 ossos, a depender do tamanho da cauda.'
+)
+
+criaCartao(
+    'Veterinária',
+    'Quantos dentes tem a arcada dentária de um cavalo?',
+    'A dentição definitiva varia de 36 a 44 dentes.'
+)
+
+criaCartao(
+    'Veterinária',
+    'Qual a espécie mais comum entre animais terrestres?',
+    'O mais comum dos animais terrestres certamente é o cachorro.'
+)
+
+criaCartao(
+    'Veterinária',
+    'Que doenças de pele são transmitidas do animal ao homem?',
+    'Toxoplasmose, Anciolostomíase, Raiva, Dipilidiose, Leptospirose, Dermatomicose, Esporotricose, Psitacose..etc'
+)
+
